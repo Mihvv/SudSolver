67179e219b8c2923300f8d0c50a1afbc59847fd5
mihv <mihvd@proton.me>
2026-06-01 19:37:31 +0200

    feat: add main.dart and fix board history not saving

--- lib/backend/utils/hive_init.dart
+++ lib/backend/utils/hive_init.dart
@@ -13,5 +13,37 @@ class HiveInit {
     if (!Hive.isAdapterRegistered(0)) {
       Hive.registerAdapter(SudokuRecordAdapter());
     }
+    if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(_DurationAdapter());
+    }
+    if (!Hive.isAdapterRegistered(2)) {
+      Hive.registerAdapter(_SolveModeAdapter());
+    }
   }
+}
+
+class _DurationAdapter extends TypeAdapter<Duration> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Duration read(BinaryReader reader) =>
+      Duration(microseconds: reader.readInt());
+
+  @override
+  void write(BinaryWriter writer, Duration obj) =>
+      writer.writeInt(obj.inMicroseconds);
+}
+
+class _SolveModeAdapter extends TypeAdapter<SolveModeRecord> {
+  @override
+  final int typeId = 2;
+
+  @override
+  SolveModeRecord read(BinaryReader reader) =>
+      SolveModeRecord.values[reader.readByte()];
+
+  @override
+  void write(BinaryWriter writer, SolveModeRecord obj) =>
+      writer.writeByte(obj.index);
 }
